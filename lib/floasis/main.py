20e8afca5a40427363fda3aeaee9637056494459
Andrew Look <andrew.m.look@gmail.com>
2018-08-23 11:53:43 -0700

    cleanup setup code

--- lib/floasis/main.py
+++ lib/floasis/main.py
@@ -13,6 +13,32 @@ from lib.floasis.inputs.rotary import RotaryEncoder
 from lib.floasis.inputs.joystick import JoystickManager
 
 
+def setup_rotary():
+    pin_ccw = Button(ROTARY_PINID_COUNTERCLOCKWISE, pull_up=True)
+    pin_cw = Button(ROTARY_PINID_CLOCKWISE, pull_up=True)
+    rotary_encoder = RotaryEncoder(pin_ccw, pin_cw,
+                                   max_value=2.0,
+                                   step_size=0.1)
+    return rotary_encoder
+
+
+def setup_joystick():
+    updown = IncrementorManager(name='scale_0', min_value=1, max_value=80,
+                                default_value=10, step_size=1)
+    leftright = IncrementorManager(name='scale_1', min_value=1, max_value=80,
+                                default_value=10, step_size=1)
+    joystick_up = Button(JOYSTICK_PINID_UP)
+    joystick_down = Button(JOYSTICK_PINID_DOWN)
+    joystick_left = Button(JOYSTICK_PINID_LEFT)
+    joystick_right = Button(JOYSTICK_PINID_RIGHT)
+    joystick_mgr = JoystickManager(btn_up=joystick_up,
+                                   btn_down=joystick_down,
+                                   btn_left=joystick_left,
+                                   btn_right=joystick_right,
+                                   mgr_updown=updown,
+                                   mgr_leftright=leftright)
+    return updown, leftright
+
 if __name__ == '__main__':
     parser = renderer2d_argparser()
     args = parser.parse_args()
@@ -25,44 +51,15 @@ if __name__ == '__main__':
             print('error connecting')
             time.sleep(5)
     
-    pin_ccw = Button(ROTARY_PINID_COUNTERCLOCKWISE, pull_up=True)
-    pin_cw = Button(ROTARY_PINID_CLOCKWISE, pull_up=True)
-    rotary_encoder = RotaryEncoder(pin_ccw, pin_cw,
-                                   max_value=2.0,
-                                   step_size=0.1)
-
-    joystick_up = Button(JOYSTICK_PINID_UP)
-    joystick_down = Button(JOYSTICK_PINID_DOWN)
-    joystick_left = Button(JOYSTICK_PINID_LEFT)
-    joystick_right = Button(JOYSTICK_PINID_RIGHT)
-
-    updown = IncrementorManager(name='scale_0', min_value=1, max_value=80,
-                                default_value=10, step_size=1)
-    leftright = IncrementorManager(name='scale_1', min_value=1, max_value=80,
-                                default_value=10, step_size=1)
-
-    joystick_mgr = JoystickManager(btn_up=joystick_up,
-                         btn_down=joystick_down,
-                         btn_left=joystick_left,
-                         btn_right=joystick_right,
-                         mgr_updown=updown,
-                         mgr_leftright=leftright)
-
+    rotary_encoder = setup_rotary()
+    updown, leftright = setu_joystick()
 
     anim = Animator(_renderer=renderer2d,
                     speed_coef_mgr=rotary_encoder,
-                   scale_0_mgr=leftright,
-                   scale_1_mgr=updown)
-
-#     input_handler = InputHandler(
-#         rotary_callback=anim.update_speed_coef,
-#         up_callback=anim.update_scale_0,
-#         left_callback=anim.update_scale_1,
-#         right_callback=anim.update_scale_2,
-#         down_callback=anim.next_anim,
-#     )
+                    scale_0_mgr=leftright,
+                    scale_1_mgr=updown)
 
     while True:
         anim.draw()
-        time.sleep(0.2)
+        time.sleep(renderer2d.tick)
 
