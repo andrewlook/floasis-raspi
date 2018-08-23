f1b805eb6dbdaaca36fe903f7baa34ed40c68215
Andrew Look <andrew.m.look@gmail.com>
2018-08-23 09:37:44 -0700

    rotary works

--- lib/floasis/inputs/rotary.py
+++ lib/floasis/inputs/rotary.py
@@ -8,38 +8,36 @@ class RotaryEncoder(IncrementorManager):
 
     def __init__(self, pin_ccw, pin_cw, **kwargs):
         super(RotaryEncoder, self).__init__(**kwargs)
-        #self._ascending = True
+        self._ascending = True
         self.pin_ccw = pin_ccw
         self.pin_cw = pin_cw
-        self.pin_ccw.when_pressed = self.update_callback(ORDER_UP)
-        self.pin_cw.when_pressed = self.update_callback(ORDER_DOWN)
+        self.pin_ccw.when_pressed = self.ccw
+        self.pin_cw.when_pressed = self.cw
 
-#     def revert_check(self):
-#         if self.value > 100:
-#             self._ascending = not self._ascending
-#             print('REVERT {i}'.format(i=self._ascending))
-#         elif self._cnt < -100:
-#             self._ascending = not self._ascending
-#             print('REVERT {i}'.format(i=self._ascending))
-# 
-#     def ccw(self):
-#         if pin_cw.is_pressed:
-#             self.revert_check()
-#             self._cnt = self._cnt + 1 if self._ascending else self._cnt - 1
-#             print('ccw {i}'.format(i=self._cnt))
-# 
-#     def cw(self):
-#         if pin_ccw.is_pressed:
-#             self.revert_check()
-#             self._cnt = self._cnt - 1 if self._ascending else self._cnt + 1
-#             print('cw {i}'.format(i=self._cnt))
-
-    @property
-    def value(self):
-        return self.value
+    def revert_check(self):
+        if self.val + self.step_size >= self.max_value + 1e-4:
+            self._ascending = not self._ascending
+            print('REVERT {i}'.format(i=self._ascending))
+        elif self.val - self.step_size <= self.min_value - 1e-4:
+            self._ascending = not self._ascending
+            print('REVERT {i}'.format(i=self._ascending))
+ 
+    def ccw(self):
+        if pin_cw.is_pressed:
+            self.revert_check()
+            direction = ORDER_UP if self._ascending else ORDER_DOWN
+            self.update(direction)
+            print('ccw {i}'.format(i=self.val))
+ 
+    def cw(self):
+        if pin_ccw.is_pressed:
+            self.revert_check()
+            direction = ORDER_DOWN if self._ascending else ORDER_UP
+            self.update(direction)
+            print('cw {i}'.format(i=self.val))
 
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     pin_ccw = Button(ROTARY_PINID_COUNTERCLOCKWISE, pull_up=True)
     pin_cw = Button(ROTARY_PINID_CLOCKWISE, pull_up=True)
 
