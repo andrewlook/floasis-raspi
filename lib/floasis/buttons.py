42967c658e050bf6f205242681d561c65b4f4100
davidbcook <david.bryn.cook@gmail.com>
2018-08-27 15:03:14 -0700

    implementing PINID defs

--- lib/floasis/buttons.py
+++ lib/floasis/buttons.py
@@ -1,10 +1,11 @@
 from gpiozero import Button
 from time import sleep
+from lib.floasis.config import *
 
-red_button = Button(21, bounce_time=0.05)
-blue_button = Button(26, bounce_time=0.05)
-grn_button = Button(19, bounce_time=0.05)
-whi_button = Button(20, bounce_time=0.05)
+red_button = Button(BUTTON_PINID_RED, bounce_time=0.05)
+blu_button = Button(BUTTON_PINID_BLU, bounce_time=0.05)
+grn_button = Button(BUTTON_PINID_GRN, bounce_time=0.05)
+whi_button = Button(BUTTON_PINID_WHI, bounce_time=0.05)
 
 def set_button_callbacks(_btn, name):
     def __callback(_status):
@@ -16,7 +17,7 @@ def set_button_callbacks(_btn, name):
     return _btn
 
 set_button_callbacks(grn_button, 'grn')
-set_button_callbacks(blue_button, 'blue')
+set_button_callbacks(blu_button, 'blu')
 set_button_callbacks(red_button, 'red')
 set_button_callbacks(whi_button, 'whi')
 
@@ -24,8 +25,8 @@ set_button_callbacks(whi_button, 'whi')
 # grn_button.when_pressed = set_button_callbacks('pressed grn\n')
 # grn_button.when_released = set_button_callbacks('depressed grn\n')
 # 
-# blue_button.when_pressed = set_button_callbacks('pressed blue\n')
-# blue_button.when_released = set_button_callbacks('depressed blue\n')
+# blu_button.when_pressed = set_button_callbacks('pressed blu\n')
+# blu_button.when_released = set_button_callbacks('depressed blu\n')
 # 
 # red_button.when_pressed = set_button_callbacks('pressed red\n')
 # red_button.when_released = set_button_callbacks('depressed red\n')
