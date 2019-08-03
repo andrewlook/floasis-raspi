7e8060f5297ecbfc10d88ea852a4d7faf52f754f
Andrew Look <look@pinterest.com>
2019-08-03 14:39:22 -0700

    run one Fadecandy script using start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-$(pwd)/fadecandy/bin/fcserver-rpi $(pwd)/configs/1024_two_strips.json
+# $(pwd)/fadecandy/bin/fcserver-rpi $(pwd)/configs/1024_two_strips.json
+$(pwd)/fadecandy/bin/fcserver-rpi $(pwd)/configs/one_strip.json
