5a80faf0168e2edd1398cf20b4e92f764686dcd6
Andrew Look <look@pinterest.com>
2019-08-06 20:50:53 -0700

    anim_test

---
+++ anim_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export LED_MAPPING="$(pwd)/configs/floasis_2d_8x8.tsv"
+export FADECANDY_HOST="raspberrypi0.local"
+
+PYTHONPATH=$(pwd) python3 $(pwd)/lib/floasis/main.py \
+    --led-cfg=${LED_MAPPING} \
+    --led-num=64 \
+    --width=8 \
+    --height=7 \
+    --tick=0.2 \
+    --host=${FADECANDY_HOST}
