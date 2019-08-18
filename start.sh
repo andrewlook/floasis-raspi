d389c7d765b5ad615860619aaf0fee6eb33ac50f
Andrew Look <look@pinterest.com>
2019-08-17 18:55:13 -0700

    use 2 strips, fix the service file for anim_all

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# $(pwd)/fadecandy/bin/fcserver-rpi $(pwd)/configs/1024_two_strips.json
-$(pwd)/fadecandy/bin/fcserver-rpi $(pwd)/configs/fc_json/one_strip.json
+$(pwd)/fadecandy/bin/fcserver-rpi $(pwd)/configs/fc_json/1024_two_strips.json
+#$(pwd)/fadecandy/bin/fcserver-rpi $(pwd)/configs/fc_json/one_strip.json
